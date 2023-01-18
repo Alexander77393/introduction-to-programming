5ece16b3c6dd2396ecde256016a1471d940b9b04
Alexander <rooms90210@gmail.com>
2023-01-18 17:55:01 +0300

    Example002 ZD3

---
+++ example003_ZD3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+int count = -numberA;
+while(count <= numberA){
+    Console.Write($"{count} ");
+    count++;
+}
